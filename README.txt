b04a6b9b88be54bffcc4a8e9f0c877b14ba3f91e
charleycorneil <146792079+charleycorneil@users.noreply.github.com>
2024-09-10 15:58:11 -0400

    First Upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+Name: Charley Corneil
+Date: 9/10/2024
+live URL: 
+Description: 
+This is my homework-one assigment 
---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById("ticketNum").innerHTML = " 3 ";
+
+document.getElementById("ticketCost").innerHTML = " 14 ";
+
+document.getElementById("ttCost").innerHTML += " 42 " + 42;
+
+document.getElementById("bank").innerHTML = " $65.87 ";
+
+document.getElementById("addJacket").innerHTML = " False ";
+
+document.getElementById("profPizza").innerHTML = "Students: 12, Rector: 2 ";
+
+document.getElementById("Monty").innerHTML = " $34.5 ";
+
+document.getElementById("tips").innerHTML = " $193.0675 ";
